059b0940eaf78a560edf76f1423c8bd4ebaeca73
DharaniGopal <dharanigopal100897@gmail.com>
2024-12-18 19:08:46 +0530

    Episode 1 and Episode 2

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/* This the shape using html for understanding to execute below code.
+<div id="parent">
+  <div id="child1">
+    <h1>Hi h1</h1>
+    <h2>Hi h2</h2>
+  </div>
+  <div id="child2">
+    <h1>Hi h1</h1>
+    <h2>Hi h2</h2>
+  </div>
+</div>
+*/
+
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "h1"),
-    React.createElement("h2", {}, "h2"),
+    React.createElement("h1", {}, "Hi h1"),
+    React.createElement("h2", {}, "Hi h2"),
   ]),
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "h1"),
-    React.createElement("h2", {}, "h2"),
+    React.createElement("h1", {}, "Hi h1"),
+    React.createElement("h2", {}, "Hi h2"),
   ]),
 ]);
 
