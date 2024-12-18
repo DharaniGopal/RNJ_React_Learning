ff15abe27bd1e6e66bb9d3c99446b82a08eea468
DharaniGopal <131185167+DharaniGopal@users.noreply.github.com>
2024-12-18 12:34:58 +0530

    Update App.js

--- Ep1/App.js
+++ Ep1/App.js
@@ -1,3 +1,16 @@
+/* This the shape using html for understanding to execute below code.
+<div id="parent">
+  <div id="child1">
+    <h1>Hi h1</h1>
+    <h2>Hi h2</h2>
+  </div>
+  <div id="child2">
+    <h1>Hi h1</h1>
+    <h2>Hi h2</h2>
+  </div>
+</div>
+*/
+
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "h1"),
