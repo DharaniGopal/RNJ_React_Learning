0fae7e62d3e9a7e449a75935e8f4ce6faba85ecb
DharaniGopal <131185167+DharaniGopal@users.noreply.github.com>
2024-12-18 12:34:23 +0530

    Update App.js

--- Ep1/App.js
+++ Ep1/App.js
@@ -1,1 +1,16 @@
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "h1"),
+    React.createElement("h2", {}, "h2"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "h1"),
+    React.createElement("h2", {}, "h2"),
+  ]),
+]);
 
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
