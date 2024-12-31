b10ad53673e8f81efc7359480846644701be7e94
DharaniGopal <131185167+DharaniGopal@users.noreply.github.com>
2024-12-31 21:39:48 +0530

    Update Header.js

--- Ep5/src/components/Header.js
+++ Ep5/src/components/Header.js
@@ -1,1 +1,22 @@
+import Logo from "../../image/logo.jpeg";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-logo">
+        <img className="logo-img" alt="logo-img" src={Logo} />
+      </div>
+      <div className="header-menus">
+        <ul>
+          <li>Home</li>
+          <li>Offer</li>
+          <li>Help</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
 
